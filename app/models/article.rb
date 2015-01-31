6bc9fafb37126a6193fb3a807357eea64eb18569
truongtr <truongtr@me.com>
2015-01-31 10:01:45 -0500

    init

---
+++ app/models/article.rb
@@ -0,0 +1,11 @@
+class Article < ActiveRecord::Base
+  validates_presence_of :title, :body
+  
+  belongs_to :user 
+  has_and_belongs_to_many :categories
+
+  def long_title
+    "#{title} #{body}"
+  end
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'f47b420d3084006dbe12d82f101e5a62ab5d90ea648fc8e027efe8ecafb73ee61f24030e9548ac634ba32403c23f3112d23808e7c015cc72b6a03c64ebe67356'
